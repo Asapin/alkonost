850db5559fc537bc515117dda34d9486d610dfbc
Vladimir Kuzmin <1559761+Asapin@users.noreply.github.com>
2021-12-06 21:19:22 +0900

    Removed unused reimport

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -5,7 +5,6 @@ use tokio::{sync::mpsc::Sender, task::JoinHandle};
 pub use tracing::info as tracing_info;
 pub use tracing::warn as tracing_warn;
 pub use tracing::error as tracing_error;
-pub use vec1::Vec1 as Vec1;
 
 pub mod detector_params;
 pub mod http_client;
